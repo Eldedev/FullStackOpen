c4355448cb7e0c01bb8dae730392328c66c9acd7
Elde <44910837+Eldedev@users.noreply.github.com>
2025-12-17 23:39:07 +0200

    osa3 välitalennus

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const morgan = require("morgan")
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
+
+morgan.token("content", (req) => {
+    if (req.method === "POST") return JSON.stringify(req.body)
+    else return " "
+})
 
 let persons = [
     {
@@ -90,7 +100,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
