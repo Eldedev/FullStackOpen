db3e722d235e9e7e7e8ac85b00049aba2679a805
Elde <44910837+Eldedev@users.noreply.github.com>
2025-12-22 11:08:41 +0200

    uibuild

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -99,7 +99,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 80
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on ${PORT}`)
 })
